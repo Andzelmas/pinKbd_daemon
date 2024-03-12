96ca5aa7e12ba6d91c8a93699c382dc85e21a84e
Andzelmas <brukstus@gmail.com>
2024-03-12 14:46:14 +0200

    Added uinput functionality. Now in testing phase, need to implement reading of Pisound-Micro and Raspberry Pi GPIO to finish

---
+++ pinKbd_daemon.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <linux/uinput.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+//var and function to catch SIGTERM when the program is killed
+volatile sig_atomic_t done = 0;
+static void term(int signum){
+    done = 1;
+}
+//function that writes bits to the fd to simulate a keypress
+static void emit(int fd, int type, int code, int val){
+    struct input_event ie;
+    ie.type = type;
+    ie.code = code;
+    ie.value = val;
+    ie.time.tv_sec = 0;
+    ie.time.tv_usec = 0;
+
+    write(fd, &ie, sizeof(ie));
+}
+
+int main(){
+    struct sigaction sig_action;
+    memset(&sig_action, 0, sizeof(struct sigaction));
+    sig_action.sa_handler = term;
+    sigaction(SIGTERM, &sig_action, NULL);
+
+    struct uinput_setup usetup;
+    int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+    //initiate the virtual device for emmiting the keypresses
+    ioctl(fd, UI_SET_EVBIT, EV_KEY);
+    //TODO What keys to use should be sourced from a file, where user can configure what
+    //encoders and buttons produce what keypresses
+    ioctl(fd, UI_SET_KEYBIT, KEY_1);
+    ioctl(fd, UI_SET_KEYBIT, KEY_2);
+    ioctl(fd, UI_SET_KEYBIT, KEY_3);
+    ioctl(fd, UI_SET_KEYBIT, KEY_4);
+    ioctl(fd, UI_SET_KEYBIT, KEY_5);
+    ioctl(fd, UI_SET_KEYBIT, KEY_6);
+    ioctl(fd, UI_SET_KEYBIT, KEY_7);
+    ioctl(fd, UI_SET_KEYBIT, KEY_8);
+    ioctl(fd, UI_SET_KEYBIT, KEY_LEFTSHIFT);
+    ioctl(fd, UI_SET_KEYBIT, KEY_LEFTALT);
+    ioctl(fd, UI_SET_KEYBIT, KEY_Q);
+    ioctl(fd, UI_SET_KEYBIT, KEY_W);
+    ioctl(fd, UI_SET_KEYBIT, KEY_E);
+    ioctl(fd, UI_SET_KEYBIT, KEY_R);
+    ioctl(fd, UI_SET_KEYBIT, KEY_T);
+    ioctl(fd, UI_SET_KEYBIT, KEY_Y);
+    ioctl(fd, UI_SET_KEYBIT, KEY_S);
+    ioctl(fd, UI_SET_KEYBIT, KEY_D);
+    ioctl(fd, UI_SET_KEYBIT, KEY_F);
+    ioctl(fd, UI_SET_KEYBIT, KEY_G);
+    ioctl(fd, UI_SET_KEYBIT, KEY_H);
+    ioctl(fd, UI_SET_KEYBIT, KEY_LEFTMETA);
+    ioctl(fd, UI_SET_KEYBIT, KEY_A);
+    
+    memset(&usetup, 0, sizeof(usetup));
+    usetup.id.bustype = BUS_USB;
+    usetup.id.vendor = 0x1234; // sample vendor
+    usetup.id.product = 0x5678; //sample product
+    strcpy(usetup.name, "Pisound-Micro Pin to Keypress Device");
+
+    ioctl(fd, UI_DEV_SETUP, &usetup);
+    ioctl(fd, UI_DEV_CREATE);
+
+    //sleep after initiate so that the system has time to register the device this can be deleted
+    //when the emit will emit events only when encoders or buttons are used
+    sleep(1);
+    
+    while(!done){
+	//TODO only for testing, should emit only when encoder or button is used
+	/*
+	emit(fd, EV_KEY, KEY_1, 1);
+	emit(fd, EV_SYN, SYN_REPORT, 0);
+	emit(fd, EV_KEY, KEY_1, 0);
+	emit(fd, EV_SYN, SYN_REPORT, 0);
+	*/
+	emit(fd, EV_KEY, KEY_LEFTMETA, 1);
+	emit(fd, EV_KEY, KEY_A, 1);
+	emit(fd, EV_SYN, SYN_REPORT, 0);
+	emit(fd, EV_KEY, KEY_LEFTMETA, 0);
+	emit(fd, EV_KEY, KEY_A, 0);
+	emit(fd, EV_SYN, SYN_REPORT, 0);
+	//sleep only for testing, so the events are not simulated too fast
+	usleep(1500000);
+    }
+    //clean everything here
+    ioctl(fd, UI_DEV_DESTROY);
+    close(fd);
+    printf("the pinKbd service is shutting down\n");
+    return 0;
+}
